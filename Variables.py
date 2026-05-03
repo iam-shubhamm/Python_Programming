0e280725f79e49f513e87778d7312d23f81c3ba6
Shubham Singh <shubhamkanpura7@gmail.com>
2026-05-04 00:22:09 +0530

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,39 @@
+a = 1  # integer number 
+print(a)
+
+b = 1.1357101 # decimal number
+print(b)
+
+c = "Shubham"  # word/sentence- use quotes
+print(c) 
+
+Shubham = 23  # declare a varibale using varibale 
+d = Shubham 
+print(d)
+
+x = 10     # adding a number to a variable 
+print(x+1)
+
+p,q,r = 11,12,13  # assign multiple varibales
+print(p,q,r) 
+
+
+# *******************************rules to declare a varibales******************************* 
+
+# 1. varibale name must start with: letter or _ 
+_my_name = "Shubham"
+print(_my_name)
+
+
+# 2. Varibale name can contain: letter, numbers, underscore (_)
+my_name1 = "Shubham"
+r1 = 26 
+_1 = 1
+
+# 3. Variables are case sensitive 
+myname = "Shubham"
+print(myname) 
+
+# 4. variables names can't be a reserved word 
+# for = 10  # for is a reserved word in python  
+ 

64feca77986502b91b59af258ecd1be3b909c4e8
Shubham Singh <shubhamkanpura7@gmail.com>
2026-05-15 23:46:30 +0530

    Add String_Indexing.py with basic string examples

---
+++ String_Indexing.py
@@ -0,0 +1,14 @@
+#************************************** String Indexing*****************************************
+
+# strings- chars in single, double and triple quotes
+name = "Shubham"     # creating a string
+print(name)
+
+print(type(name))   # checking data type
+
+print("It's easy")
+print("Hello World")
+
+print(''' "kw-double Quotes" ''') 
+
+print(" \"kw-double Quotes\" ") 

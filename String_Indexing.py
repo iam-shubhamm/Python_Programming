938fb30f6c763ed993b230286462fa79b2558b67
Shubham Singh <shubhamkanpura7@gmail.com>
2026-05-15 23:47:46 +0530

    Add examples of f-strings and escape characters

--- String_Indexing.py
+++ String_Indexing.py
@@ -34,3 +34,22 @@ print("My name is {0} and I'm {1}".format(name, age))
 print("My name is {1} and I'm {0}".format(name, age))
 
 print("My name is {name} and I'm {age}".format(name="Keshav", age="21"))
+
+#3. f-strings 
+
+name = "Singh"
+age = 23
+print(f"My name is {name} and I'm {age}")
+
+print(f"My age after 5 years will be {age + 5}") 
+
+
+# Escape Characters - backslash with chars 
+print(''' "kw-double Quotes" ''') 
+
+print(" \"kw-double Quotes\" ")  # double quotes using \
+
+print(" \'kw-single Quotes\' ") # single quote using \
+
+print("Hello\nWorld")       # new line
+print("Hello\tWorld")       # tab - space 

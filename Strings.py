da4f731e94a4cfd02f70c9698374db1bf6b9bd86
Shubham Singh <shubhamkanpura7@gmail.com>
2026-05-15 22:10:36 +0530

    Update and rename Strings to Strings.py

---
+++ Strings.py
@@ -0,0 +1,37 @@
+#*******************************Strings in Python************************************
+
+# strings- chars in single, double and triple quotes
+
+name = "Shubham"     # creating a string
+print(name)
+
+print(type(name))   # checking data type
+
+print("It's easy")
+print("Hello World")
+
+print(''' "kw-double Quotes" ''') 
+
+print(" \"kw-double Quotes\" ") 
+
+# Formatted Strings - insert variables or experssions
+#1. Old style formatting - % operator 
+
+name = "Singh"
+age = 16 
+print("My name is %s and I'm %d" % (name, age)) 
+# %s, %d are placeholders for the string and int 
+
+
+#2. str.format() method 
+
+name = "Shubham"
+age = 16 
+print("My name is {} and I'm {}".format(name, age)) 
+
+
+# you can reference variables by index or keyword
+print("My name is {0} and I'm {1}".format(name, age))
+print("My name is {1} and I'm {0}".format(name, age))
+
+print("My name is {name} and I'm {age}".format(name="Shivam", age="19"))

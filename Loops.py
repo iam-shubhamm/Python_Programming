1b2e09dabf9ef3e449b34cad313f0de2acc68d39
Shubham Singh <shubhamkanpura7@gmail.com>
2026-05-16 11:42:54 +0530

    Implement examples of while loops in Loops.py
    Added examples of while loops in Python demonstrating counting up and down.

---
+++ Loops.py
@@ -0,0 +1,33 @@
+#*************************************Loops in Python**********************************************  
+
+# while loop 
+
+count = 0 
+
+while count < 5: #condition 
+    print(count)
+    count = count + 1
+
+
+# print numbers from 1 to 5 using while loop
+count = 1 
+while count < 6: #condition 
+    print(count)
+    # count = count + 1
+    count += 1 
+
+
+count = 5 
+while count > 0: #condition 
+    print(count)
+    # count = count + 1
+    count -= 1 
+else:
+    print("while loop ended")
+
+
+# while True:
+#     print("again and again!!") 
+# check conditions to avoid infinite loop
+
+
